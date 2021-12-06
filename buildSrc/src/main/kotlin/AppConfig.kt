5fc03ea9793df479f43fcd14d1fb58e535f3c63c
keronei <lincolnkeronei95@gmail.com>
2021-12-06 15:04:34 +0300

    Bumped version number and changed orientation of radio buttons to vertical to support smaller screens

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 30
     const val minSdk = 21
     const val targetSdk = 30
-    const val versionCode = 2
-    const val versionName = "0.4"
+    const val versionCode = 3
+    const val versionName = "0.8"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
