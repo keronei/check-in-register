38d84319604e06acf2eda1ce09c2e5788bfc018b
keronei <lincolnkeronei95@gmail.com>
2021-12-06 15:55:09 +0300

    Changed member & region name font family

--- app/src/main/java/com/keronei/keroscheckin/MainActivity.kt
+++ app/src/main/java/com/keronei/keroscheckin/MainActivity.kt
@@ -59,6 +59,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun applyFont(){
+
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(com.keronei.keroscheckin.R.menu.menu_main, menu)
