aa1ac270af6fd2caca87af150c9372e4380c1f38
keronei <lincolnkeronei95@gmail.com>
2022-02-11 16:37:21 +0300

    Fixed navigation to import stage

--- app/src/main/java/com/keronei/keroscheckin/fragments/importexport/MergePromptImports.kt
+++ app/src/main/java/com/keronei/keroscheckin/fragments/importexport/MergePromptImports.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.keronei.keroscheckin.R
 import com.keronei.keroscheckin.databinding.BottomModalMergeImportsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
 @AndroidEntryPoint
 class MergePromptImports : BottomSheetDialogFragment() {
@@ -24,7 +27,38 @@ class MergePromptImports : BottomSheetDialogFragment() {
         importBindingOptions =
             DataBindingUtil.inflate(inflater, R.layout.bottom_modal_merge_imports, container, false)
 
+        setOnclickListeners()
+
         return importBindingOptions.root
+    }
+
+
+    private fun setOnclickListeners() {
+        importBindingOptions.replace.setOnClickListener {
+            MotionToast.createToast(
+                requireActivity(),
+                "Success",
+                "Import Completed successfully!",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(requireActivity(), R.font.helvetica_regular)
+            )
+
+        }
+
+        importBindingOptions.merge.setOnClickListener {
+            MotionToast.createToast(
+                requireActivity(),
+                "Success",
+                "All entries saved.",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(requireActivity(), R.font.helvetica_regular)
+            )
+
+        }
     }
 
     companion object {
