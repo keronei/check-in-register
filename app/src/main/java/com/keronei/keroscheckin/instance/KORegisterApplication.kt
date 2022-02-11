6c2afa0e44511d1122633af04af61afcc3b7366a
keronei <lincolnkeronei95@gmail.com>
2022-02-11 16:34:20 +0300

    Planted timber

--- app/src/main/java/com/keronei/keroscheckin/instance/KORegisterApplication.kt
+++ app/src/main/java/com/keronei/keroscheckin/instance/KORegisterApplication.kt
@@ -1,7 +1,9 @@
 package com.keronei.keroscheckin.instance
 
 import android.app.Application
+import com.keronei.keroscheckin.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class KORegisterApplication : Application() {
@@ -9,6 +11,10 @@ class KORegisterApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     companion object {
