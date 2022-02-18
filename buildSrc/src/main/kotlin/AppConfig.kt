0411aa0ee05818d43701b6d8ef0bdf00988245c4
keronei <lincolnkeronei95@gmail.com>
2022-02-18 11:54:10 +0300

    Bumped app version & code

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 31
     const val minSdk = 21
     const val targetSdk = 31
-    const val versionCode = 4
-    const val versionName = "0.9"
+    const val versionCode = 5
+    const val versionName = "1.0"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
